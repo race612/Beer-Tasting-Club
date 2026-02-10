e2a9156d25400ccc13ca6ffb2dcbb020273ba651
race612 <mirko.caiani@gmail.com>
2026-02-10 10:03:54 +0000

    new version 2

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // js/config.js
-export const APP_VERSION = '0.5.4 Alpha';
+export const APP_VERSION = '0.5.5 Alpha';
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // IMPORTANTE: Cambia questa stringa ad ogni rilascio per forzare l'aggiornamento sugli utenti
-const CACHE_NAME = 'btc-cache-v0.5.4-alpha'; 
+const CACHE_NAME = 'btc-cache-v0.5.5-alpha'; 
 
 const ASSETS_TO_CACHE = [
   './',
