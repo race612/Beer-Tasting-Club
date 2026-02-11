5a2dcea9699a9937e7b06b80a42b4e429858ee97
race612 <mirko.caiani@gmail.com>
2026-02-11 11:28:50 +0000

    Continui miglioramenti scheda bjcp completa

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // IMPORTANTE: Cambia questa stringa ad ogni rilascio per forzare l'aggiornamento sugli utenti
-const CACHE_NAME = 'btc-cache-v0.6.2.2-alpha'; 
+const CACHE_NAME = 'btc-cache-v0.6.2.3-alpha'; 
 
 const ASSETS_TO_CACHE = [
   './',
