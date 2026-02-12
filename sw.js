4b4c576635e7e715ca05e72c25a39c8636f121b2
race612 <mirko.caiani@gmail.com>
2026-02-12 13:48:54 +0000

    Bjcp Complete improvements

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // IMPORTANTE: Cambia questa stringa ad ogni rilascio per forzare l'aggiornamento sugli utenti
-const CACHE_NAME = 'btc-cache-v0.6.3-alpha'; 
+const CACHE_NAME = 'btc-cache-v0.6.4-alpha'; 
 
 const ASSETS_TO_CACHE = [
   './',
