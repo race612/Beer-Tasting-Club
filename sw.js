62fee81f78fd64f147ace1df1d9f85430ebc04e6
race612 <mirko.caiani@gmail.com>
2026-02-14 01:28:13 +0100

    Css Version correction

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // IMPORTANTE: Cambia questa stringa ad ogni rilascio per forzare l'aggiornamento sugli utenti
-const CACHE_NAME = 'btc-cache-v0.6.14-alpha'; 
+const CACHE_NAME = 'btc-cache-v0.6.15';
 
 const ASSETS_TO_CACHE = [
   './',
