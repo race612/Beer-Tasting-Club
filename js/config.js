a16707445d31d75191c01772daaf438e12e854f7
Mirko Caiani <mirko.caiani@gmail.com>
2026-02-20 17:27:27 +0100

    v0.7.4

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // js/config.js
-export const APP_VERSION = '0.7.3';
+export const APP_VERSION = '0.7.4';
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // IMPORTANTE: Cambia questa stringa ad ogni rilascio per forzare l'aggiornamento sugli utenti
-const CACHE_NAME = 'btc-cache-v0.7.3';
+const CACHE_NAME = 'btc-cache-v0.7.4';
 
 const ASSETS_TO_CACHE = [
   './',
