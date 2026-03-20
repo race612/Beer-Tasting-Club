8822231ff5443e24ab491121017a2a7d8f272dc3
Mirko Caiani <mirko.caiani@gmail.com>
2026-03-20 15:34:43 +0100

    v0.7.26

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 // IMPORTANTE: Cambia questa stringa ad ogni rilascio per forzare l'aggiornamento sugli utenti
-const CACHE_NAME = 'btc-cache-v0.7.25';
+const CACHE_NAME = 'btc-cache-v0.7.26';
 
 const ASSETS_TO_CACHE = [
   './',
